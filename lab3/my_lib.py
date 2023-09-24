2c6ecb02c6736b1be8bf01fa80940619ebb097f4
Drobysh Dmitry <90309201+NorthCapDiamond@users.noreply.github.com>
2023-09-25 02:00:45 +0300

    Delete lab3 directory

--- lab3/my_lib.py
+++
@@ -1,6 +0,0 @@
-def merge_underlined(array):
-	if (len(array)==1):
-		return array[0]
-	else:
-		answer = "_".join(array)
-		return answer
--- lab3/user_io.py
+++
@@ -1,9 +0,0 @@
-def checked_input(message = ""):
-	user_line =""
-	try:
-		user_line = input(message+"\n")
-	except EOFError:
-		print("Invalid input try again...")
-		checked_input(message)
-	return user_line
-
